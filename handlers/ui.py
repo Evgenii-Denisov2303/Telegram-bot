32eed24f1eb8578a0c332c85f3e151cbb742ec47
Evgenii Denisov <denisov.jeka@gmail.com>
2026-01-21 05:51:23 +0300

    Окончательный новый дизайн

--- handlers/ui.py
+++ handlers/ui.py
@@ -1,3 +1,6 @@
+from aiogram.exceptions import TelegramBadRequest
+
+
 async def send_or_update_hub(message, text, markup, ui_state):
     chat_id = message.chat.id
     message_id = ui_state.get(message.from_user.id)
@@ -10,8 +13,9 @@ async def send_or_update_hub(message, text, markup, ui_state):
                 reply_markup=markup,
             )
             return
-        except TelegramBadRequest:
-            pass
+        except TelegramBadRequest as exc:
+            if "message is not modified" in str(exc):
+                return
 
     sent = await message.answer(text, reply_markup=markup)
     ui_state[message.from_user.id] = sent.message_id
