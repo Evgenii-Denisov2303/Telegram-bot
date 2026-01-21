77b19d0f7148b9ef3aa9e374806c7dbda8b5f370
Evgenii-Denisov2303 <denisov.jeka@gmail.com>
2026-01-21 08:26:56 +0300

    Update ui.py

--- handlers/ui.py
+++ handlers/ui.py
@@ -1,22 +1,27 @@
 from aiogram.exceptions import TelegramBadRequest
 
 
-async def send_or_update_hub(message, text, markup, ui_state, reply_keyboard=None):
-    chat_id = message.chat.id
-    message_id = ui_state.get(message.from_user.id)
-    if message_id:
-        try:
-            await message.bot.edit_message_text(
-                text=text,
-                chat_id=chat_id,
-                message_id=message_id,
-                reply_markup=markup,
-            )
-            return
-        except TelegramBadRequest as exc:
-            if "message is not modified" in str(exc):
+async def send_or_update_hub(
+    message,
+    text,
+    markup,
+    ui_state,
+    reply_keyboard=None,
+    force_new: bool = False
+):
+    if not force_new:
+        message_id = ui_state.get(message.from_user.id)
+        if message_id:
+            try:
+                await message.bot.edit_message_text(
+                    text=text,
+                    chat_id=message.chat.id,
+                    message_id=message_id,
+                    reply_markup=markup,
+                )
                 return
+            except:
+                pass
 
-    reply_markup = markup if markup is not None else reply_keyboard
-    sent = await message.answer(text, reply_markup=reply_markup)
+    sent = await message.answer(text, reply_markup=markup)
     ui_state[message.from_user.id] = sent.message_id
